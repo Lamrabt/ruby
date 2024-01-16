68ee9e78fa70d9f7a1df8e2f6adf2af3dd928851
lamrabt <lamrabtyassin3@gmail.com>
2024-01-16 16:29:06 +0100

    projet validant 2

--- my-first-programe.rb
+++
@@ -1,9 +0,0 @@
-puts "Bonjour, vous êtes combien ?"
-print "> "
-nombre = gets.chomp.to_i
-
-nombre.times do
-    puts i+1
-    i=i+1
-end
-\\wsl.localhost\Ubuntu-20.04\home\yassin\cours-code\ruby\exo-17.rb
